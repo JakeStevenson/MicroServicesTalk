a26e5764d681a366abac5053dd76f707be920cd6
Jake A. Stevenson <jake.stevenson@gmail.com>
2015-06-16 16:31:45 -0600

    Build script

---
+++ build.ps1
@@ -0,0 +1,3 @@
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe .\AccountManagement\AccountManagement.sln
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe .\Billing\Billing.sln
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe .\Communications\Communications.sln
