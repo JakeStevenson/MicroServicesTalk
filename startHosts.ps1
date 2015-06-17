decc27d4d24c7295d4236fd8d05bccddfc8e1a37
Jake A. Stevenson <jake.stevenson@gmail.com>
2015-06-17 09:28:03 -0600

    Script to start hosts

---
+++ startHosts.ps1
@@ -0,0 +1,3 @@
+& .\AccountManagement\AccountManagement.Host\bin\Debug\NServiceBus.Host.exe -new_console:sV
+& .\Billing\Billing.Host\bin\Debug\NServiceBus.Host.exe -new_console:sV
+& .\Communications\Communications.Host\bin\Debug\NServiceBus.Host.exe -new_console:sV
