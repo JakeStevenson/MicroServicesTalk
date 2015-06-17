3953f37f801a737e2a997c6f537a9866c8722d4b
Jake A. Stevenson <jake.stevenson@gmail.com>
2015-06-17 11:36:05 -0600

    Use installer to ensure queues

--- startHosts.ps1
+++ startHosts.ps1
@@ -1,3 +1,7 @@
+& .\AccountManagement\AccountManagement.Host\bin\Debug\NServiceBus.Host.exe /install /startManually
+& .\Billing\Billing.Host\bin\Debug\NServiceBus.Host.exe /install /startManually
+& .\Communications\Communications.Host\bin\Debug\NServiceBus.Host.exe /install /startManually
+
 & .\AccountManagement\AccountManagement.Host\bin\Debug\NServiceBus.Host.exe -new_console:sV
 & .\Billing\Billing.Host\bin\Debug\NServiceBus.Host.exe -new_console:sV
 & .\Communications\Communications.Host\bin\Debug\NServiceBus.Host.exe -new_console:sV
