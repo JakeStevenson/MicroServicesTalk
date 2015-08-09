b16d50730840d3d17a78152f97218781310b0eec
Jake A. Stevenson <jake.stevenson@gmail.com>
2015-08-09 12:46:29 -0500

    Improve hosts startup

--- startHosts.ps1
+++ startHosts.ps1
@@ -2,6 +2,6 @@
 & .\Billing\Billing.Host\bin\Debug\NServiceBus.Host.exe /install /startManually
 & .\Communications\Communications.Host\bin\Debug\NServiceBus.Host.exe /install /startManually
 
-& .\AccountManagement\AccountManagement.Host\bin\Debug\NServiceBus.Host.exe -new_console:sV
-& .\Billing\Billing.Host\bin\Debug\NServiceBus.Host.exe -new_console:sV
-& .\Communications\Communications.Host\bin\Debug\NServiceBus.Host.exe -new_console:sV
+& .\AccountManagement\AccountManagement.Host\bin\Debug\NServiceBus.Host.exe -new_console:sH
+& .\Billing\Billing.Host\bin\Debug\NServiceBus.Host.exe -new_console:s67V
+& .\Communications\Communications.Host\bin\Debug\NServiceBus.Host.exe -new_console:s33V
